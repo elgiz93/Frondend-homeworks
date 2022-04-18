4d0f35a67b7036518e5826ebdd29ee5a61e0eb0f
elgiz93 <98798356+elgiz93@users.noreply.github.com>
2022-04-18 23:26:48 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+let say = 1;
+count.innerHTML=say;
+
+function reset(){
+    say=1;
+    count.innerHTML=say;
+
+}
+
+function decrease(){
+    say--;
+    count.innerHTML=say;
+
+}
+
+function increase(){
+    say++;
+    count.innerHTML=say;
+
+}
